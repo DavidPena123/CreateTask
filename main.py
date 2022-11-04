d951fc72e97ab0b16f3cb1c7d042e5cf19ce946d
DavidPena123 <david.pena@student.rjuhsd.us>
2022-11-04 13:39:36 -0700

    Basic Movement and Boundaries done

--- main.py
+++ main.py
@@ -8,35 +8,59 @@ pygame.init()
 
  
 # Colours
-BACKGROUND = (255, 255, 255)
+BACKGROUND = (0, 0, 0)
  
 # Game Setup
 FPS = 60
 fpsClock = pygame.time.Clock()
-WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 300
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+BLACK = (0, 0, 0)
  
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('My Game!')
- 
+pygame.display.set_caption('Snake!')
+
 # The main function that controls the game
 def main () :
-  looping = True
+  #Character Attributes
+  x = 50
+  y = 50
+  width = 40
+  height = 60
+  vel = 5
   
   # The main game loop
+  looping = True
   while looping :
-    # Get inputs
+    pygame.time.delay(50)
+
     for event in pygame.event.get() :
       if event.type == QUIT:
-        pygame.quit()
-        sys.exit()
+        looping = False
     
-    # Processing
-    # This section will be built out later
- 
-    # Render elements of the game
-    WINDOW.fill(BACKGROUND)
+    # Movement and Boundaries    
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT] and x > 0:
+      x -= vel
+      
+    if keys[pygame.K_RIGHT] and x < WINDOW_WIDTH - width:
+      x += vel
+      
+    if keys[pygame.K_UP] and y > 0:
+      y -= vel
+      
+    if keys[pygame.K_DOWN] and y < WINDOW_HEIGHT - height:
+      y += vel
+      
+      
+    #Fills character movement  
+    WINDOW.fill(BLACK)
+      
+    #Character
+    pygame.draw.rect(WINDOW, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
-    fpsClock.tick(FPS)
+
+    
  
 main()
