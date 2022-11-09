ad60841c89d3d274ee3581b3f54cc260e4a736e9
DavidPena123 <david.pena@student.rjuhsd.us>
2022-11-08 20:25:13 -0800

    finally figured out spawn apple, work on collision and spawning another apple now

--- main.py
+++ main.py
@@ -20,11 +20,9 @@ WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Snake!')
 
 def apple_spawn():
-    global xValue
-    global yValue
+  
     xValue = random.randint(0,500)
     yValue = random.randint(0,500)
-  
     value = xValue % 25
     value = 25 - value
     for j in range(value):
@@ -34,10 +32,10 @@ def apple_spawn():
     secondValue = 25 - secondValue
     for j in range(secondValue):
       yValue += 1
-  
-    return WINDOW.blit(apple, (xValue,yValue))
+    cordinates = [xValue, yValue]
+    return cordinates
 
-def snake_touch_apple(x, y):
+def snake_touch_apple():
   xPos = 0
   yPos = 0
   if xPos == x and yPos == y:
@@ -47,46 +45,50 @@ def snake_touch_apple(x, y):
   #if x,y touches apples position, delete apple and make a new one.
 
 def redrawGameWindow():
-  
+   
     WINDOW.blit(map, (0,0))
-    print(apple_spawn())
     pygame.display.update()
     
 def movement_boundaries(x, y, width, height, vel):
-  
+  cordinates = apple_spawn()
+  print(cordinates)
   looping = True
   while looping:
-    pygame.time.delay(50)
+    pygame.time.delay(120)
     redrawGameWindow()
     for event in pygame.event.get():
       if event.type == QUIT:
         looping = False
-    
+  
     # Movement and Boundaries    
     keys = pygame.key.get_pressed()
-    
+
     if keys[pygame.K_LEFT] and x > 0:
       x -= vel
       
-    elif keys[pygame.K_RIGHT] and x < WINDOW_WIDTH - 25:
+    elif keys[pygame.K_RIGHT] and x < WINDOW_WIDTH - width:
       x += vel
        
     elif keys[pygame.K_UP] and y > 0:
       y -= vel
       
-    elif keys[pygame.K_DOWN] and y < WINDOW_HEIGHT - 25:
+    elif keys[pygame.K_DOWN] and y < WINDOW_HEIGHT - height:
       y += vel
     
-    pygame.draw.rect(WINDOW, (0, 0, 255), (x, y, 25, 25))
-    
+    player = pygame.draw.rect(WINDOW, (0, 0, 255), (x, y, 25, 25))
+    WINDOW.blit(apple, (cordinates[0],cordinates[1]))
     pygame.display.update()
-    
+     
+     #Snake collliding with apple, score + 1
+     #Snake touches border, you lose screen,show score
+     #Snake touches itself, you lose screen,show score
+     #Press any button to start
     
 def main () :
   #Character Attributes
   x = 100
   y = 100
-  width = 125
+  width = 25
   height = 25
   vel = 25
   redrawGameWindow()
@@ -96,10 +98,7 @@ def main () :
      
    
 
-       #Snake collliding with apple, score + 1
-       #Snake touches border, you lose screen,show score
-       #Snake touches itself, you lose screen,show score
-       #Press any button to start
+
     
  
 main()
